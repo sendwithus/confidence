7737c11cd161e9e5822a49fcc57d7443686cfdff
Eric Kerr <EricPKerr@gmail.com>
2014-04-08 17:22:57 -0500

    Probability percentage approximation from the configured zScore

--- confidence.js
+++ confidence.js
@@ -27,6 +27,30 @@
   };
   
   Confidence.version = VERSION;
+  
+  var Z_MAX = 6;
+  var zScoreProbability = function(z) {
+    var w, x, y;
+    if (z === 0.0) {
+      x = 0.0;
+    } else {
+      y = 0.5 * Math.abs(z);
+      if (y > (Z_MAX * 0.5)) {
+        x = 1.0;
+      } else if (y < 1.0) {
+        w = y * y;
+        x = ((((((((0.000124818987 * w - 0.001075204047) * w + 0.005198775019) * w - 0.019198292004) * w + 0.059054035642) * w - 0.151968751364) * w + 0.319152932694) * w - 0.531923007300) * w + 0.797884560593) * y * 2.0;
+      } else {
+        y -= 2.0;
+        x = (((((((((((((-0.000045255659 * y + 0.000152529290) * y - 0.000019538132) * y - 0.000676904986) * y + 0.001390604284) * y - 0.000794620820) * y - 0.002034254874) * y + 0.006549791214) * y - 0.010557625006) * y + 0.011630447319) * y - 0.009279453341) * y + 0.005353579108) * y - 0.002141268741) * y + 0.000535310849) * y + 0.999936657524;
+      }
+    }
+    if (z > 0.0) {
+      return (x + 1) / 2.0;
+    } else {
+      return (1 - x) / 2.0;
+    }
+  }
 
   /** Public Constants **/
 
@@ -142,7 +166,9 @@
     // identify ID with the largest max and its value
     var idWithLargestMax = maximums[0].id;
     var largestMax = maximums[0].val;
-
+    
+    var confidencePercent = this.getConfidencePercent();
+    
     var result;
 
     var hasWinner;
@@ -155,7 +181,7 @@
       var roundedMin = (Math.round(10000 * confidenceIntervals[idWithLargestMin].min)/100);
       var roundedMax = (Math.round(10000 * confidenceIntervals[idWithLargestMin].max)/100);
 
-      var message = "With 95% confidence, the true population parameter of the \"";
+      var message = "With " + confidencePercent + "% confidence, the true population parameter of the \"";
       message += winningVariantName + "\" variant will fall between ";
       message += roundedMin + "% and ";
       message += roundedMax + "%.";
@@ -167,6 +193,7 @@
         hasEnoughData: hasEnoughData,
         winnerID: idWithLargestMin,
         winnerName: winningVariantName,
+        confidencePercent: confidencePercent,
         confidenceInterval: { min: roundedMin, max: roundedMax },
         readable: message
       };
@@ -183,6 +210,7 @@
         hasEnoughData: hasEnoughData,
         winnerID: null,
         winnerName: null,
+        confidencePercent: confidencePercent,
         confidenceInterval: null,
         readable: messageNoWinner
       };
@@ -199,6 +227,11 @@
     return list;
   };
 
+  // Gets Confidence percentage from the configured zscore
+  Confidence.prototype.getConfidencePercent = function() {
+    var normalProbability = zScoreProbability(this._zScore);
+    return Math.round(100 * (2 * normalProbability - 1));
+  }
 
   // Are these result statistically significant?
   // (zscore^2 * stdErr * (1 - stdErr)) / marginErr^2
